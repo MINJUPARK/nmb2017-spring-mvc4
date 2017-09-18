cf73c919d44e19374855ebcbbc436d3ff322dfc2
minju <psj0943@naver.com>
2017-09-18 23:26:03 +0900

    컨트롤러 추가

---
+++ src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
@@ -0,0 +1,16 @@
+package kr.hs.emirim.nmb2017.springmvc4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	// root 로 들어오는 것들을 처리한다.
+	@RequestMapping("/")
+	@ResponseBody
+	 public String hello() {
+	    return "안녕. 스프링!";
+	 }
+}
