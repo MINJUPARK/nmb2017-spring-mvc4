9a07d806c589e994f90a55ea1f2f4fc95124ed89
minju <psj0943@naver.com>
2017-09-20 01:33:56 +0900

    name 파라미터 처리 추가

--- src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
+++ src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
@@ -3,6 +3,7 @@ package kr.hs.emirim.nmb2017.springmvc4.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -16,8 +17,8 @@ public class HelloController {
 	//	 }
 	
 	@RequestMapping("/")
-	 public String hello(Model model) {
-		model.addAttribute("message", "Hello from the controller"); 
+	 public String hello(@RequestParam("name") String userName, Model model) {
+		model.addAttribute("message", userName + "님 안녕하세요."); // 주소창/?name=이름 을 넣어줘야 실행 가
 		  return "resultPage";
 	 }
 }
